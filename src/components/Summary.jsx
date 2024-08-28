8cf8ca0dda3d6d194a9dc6248a2a781552c79dcd
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-08-28 18:21:23 +0530

    complete final results page

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,21 +3,34 @@ import quizComplete from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions.js";
 
 const Summary = ({ userAnswers }) => {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswersShare = 100 - (skippedAnswersShare + correctAnswersShare);
+
   return (
     <div id="summary">
       <img src={quizComplete} alt="Trophy icon" />
       <h2>Quiz Completed!</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswersShare}%</span>
           <span className="text">Skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswersShare}%</span>
           <span className="text">Answered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswersShare}%</span>
           <span className="text">Answered incorrectly</span>
         </p>
       </div>
